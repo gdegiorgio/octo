6ae47425b21f9f859da1d8927ddd320aa913c166
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2025-06-11 14:44:28 +0200

    feat: implement install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 OCTO_HOME=$HOME/.octo/
 
@@ -6,12 +6,54 @@ OCTO_HOME=$HOME/.octo/
 
 install(){
 
-    if [ $1!="" ]
-    then
-        echo "No version selected, octo latest will be installed."
+
+    if [ -f $OCTO_HOME/bin/octo ]; then
+        echo "Octo binary already exists"
+        return
     fi
 
-    mkdir -p $OCTO_HOME
+    # Check if curl is installed
+    if ! command -v curl &> /dev/null; then
+        echo "curl could not be found"
+        return
+    fi
+
+    # Check os architecture
+    if [ $(uname -m) == "x86_64" ]; then
+        ARCH="amd64"
+    elif [ $(uname -m) == "aarch64" ]; then
+        ARCH="arm64"
+    else
+        echo "Unsupported architecture"
+        return
+    fi
+
+    mkdir -p $OCTO_HOME/bin
+    curl -L -o $OCTO_HOME/bin/octo https://github.com/gdegiorgio/octo/releases/latest/download/octo_linux_$ARCH
+    chmod +x $OCTO_HOME/bin/octo
+
+
+    if [ $SHELL == "/bin/bash" ]; then
+        echo 'export PATH=$PATH:$OCTO_HOME/bin' >> $HOME/.bashrc
+        source $HOME/.bashrc
+
+    elif [ $SHELL == "/bin/zsh" ]; then
+        echo 'export PATH=$PATH:$OCTO_HOME/bin' >> $HOME/.zshrc
+        source $HOME/.zshrc
+    elif [ $SHELL == "/bin/sh" ]; then
+        echo 'export PATH=$PATH:$OCTO_HOME/bin' >> $HOME/.profile
+        source $HOME/.profile
+    else
+        echo "Unsupported shell"
+        return
+    fi
+
+
+    echo "Octo installed successfully"
+
+
 }
+
+
 
 install
